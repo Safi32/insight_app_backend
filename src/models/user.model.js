3b9f13644ccd8783d7dfbd6475e44cafe0ed5159
unknown <ismailahmad.professional@gmail.com>
2025-06-10 16:28:08 +0500

    env example updated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,7 +9,10 @@ const userSchema = new mongoose.Schema(
       unique: true,
       required: true,
     },
-    password: { type: String, required: true },
+    password: { 
+      type: String, 
+      required: true 
+    },
     role: {
       type: String,
       required: true,
